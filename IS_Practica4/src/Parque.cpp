8dccb2d6bb9d53ef8e4028bf42a3fa0f0dd8e503
Fran119 <81767376+GD-Fran119@users.noreply.github.com>
2021-08-17 13:36:50 +0200

    Clase Parque terminada
    Se termina de codificar la clase Parque. No se realizan test para esta clase a causa de que sólo posee métodos setters y getters.

--- IS_Practica4/src/Parque.cpp
+++ IS_Practica4/src/Parque.cpp
@@ -5,15 +5,60 @@
  *      Author: francisco
  */
 
+#include <iostream>
 #include "Parque.h"
 #include "Date.h"
+#include "Sendero.h"
 
-Parque::Parque() {
+Parque::Parque(std::string lt, std::string lc, std::string prov, std::string nam,
+		int sur, Date dec, std::list<std::string> rec, std::list<Sendero> send):
+		fecha_declaracion(dec){
 	// TODO Auto-generated constructor stub
+	location = lt;
+	localidad = lc;
+	provincia = prov;
+	name = nam;
+	surface = sur;
+	reconocimientos = rec;
+	senderos = send;
+}
 
+
+void Parque::print_Info(){
+	std::cout << "Nombre: " << name << '\n';
+	std::cout << "Superficie: " << surface << '\n';
+	std::cout << "Ubicacion: " << location << '\n';
+	std::cout << "Localidad: " << localidad << '\n';
+	std::cout << "Provincia: " << provincia << '\n';
+	std::cout << "Superficie: " << surface << '\n';
+	std::cout << "Fecha de declaracion: " << fecha_declaracion.get_date_string() << '\n';
+
+	std::cout << "Reconocimientos: ";
+	for(auto aux : reconocimientos) std::cout << aux + " ";
+	std::cout << '\n';
+
+	std::cout << "Senderos: ";
+	for(auto aux : senderos) std::cout << aux.get_Nombre() + " ";
+	std::cout << '\n';
+
+}
+
+
+void Parque::set_Info(std::string lt, std::string lc, std::string prov, std::string nam,
+			int sur, Date dec, std::list<std::string> rec, std::list<Sendero> send){
+	location = lt;
+	localidad = lc;
+	provincia = prov;
+	name = nam;
+	surface = sur;
+	fecha_declaracion = dec;
+	reconocimientos = rec;
+	senderos = send;
 }
 
 Parque::~Parque() {
 	// TODO Auto-generated destructor stub
+	senderos.clear();
+	reconocimientos.clear();
 }
 
--- IS_Practica4/src/Parque.h
+++ IS_Practica4/src/Parque.h
@@ -8,14 +8,26 @@
 #ifndef SRC_PARQUE_H_
 #define SRC_PARQUE_H_
 #include "Date.h"
+#include "Sendero.h"
 #include <string>
+#include <list>
 
 class Parque {
 private:
-
+	std::string location, localidad, provincia, name;
+	int surface;
+	std::list<std::string> reconocimientos;
+	Date fecha_declaracion;
+	std::list<Sendero> senderos;
 
 public:
-	Parque();
+	Parque(std::string lt, std::string lc, std::string prov, std::string nam,
+			int sur, Date dec, std::list<std::string> rec, std::list<Sendero> send);
+	void print_Info();
+	inline std::list<std::string> get_Senderos(){return senderos;};
+	void set_Info(std::string lt, std::string lc, std::string prov, std::string nam,
+			int sur, Date dec, std::list<std::string> rec, std::list<Sendero> send);
+	inline std::string get_Nombre(){return name;};
 	virtual ~Parque();
 };
 
